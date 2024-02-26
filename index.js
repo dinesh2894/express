7a40d43ebdc665f4a9aa4532c70dfc74661514fa
Dinesh Saini <sainidinesh2894@gmail.com>
2024-02-26 21:33:18 +0530

    add some path /apple /orange and *

--- index.js
+++ index.js
@@ -9,7 +9,22 @@ app.listen(port,()=>{
     console.log(`App listen is port : ${port}`);
 });
 
-app.use((req, res)=>{
-    console.log(req);
-    res.send("This is response");
+// app.use((req, res)=>{
+//     console.log(req);
+//     // res.send("This is response");
+//     let code = "<h1>Fruits</h1><ul><li>Apple</li><li>Orange</li></ul>";
+//     res.send(code);
+// });
+
+app.get("/",(req,res)=>{
+    res.send('Home path');
+});
+app.get("/apple",(req,res)=>{
+    res.send('Apple path');
+});
+app.get("/orange",(req,res)=>{
+    res.send('Orange path');
+});
+app.get("*",(req,res)=>{
+    res.send('Page does not exist');
 });
